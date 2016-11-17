4bd549c01527dd27802ef05118f865c1d81388ae
DANIEL A. ST CLAIR <danielstclair@gmail.com>
2016-11-17 11:30:01 -0600

    Updated template to supprt HMR and non HMR options with node server.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 const http = require('http').Server(app);
-const io = require('socket.io');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
